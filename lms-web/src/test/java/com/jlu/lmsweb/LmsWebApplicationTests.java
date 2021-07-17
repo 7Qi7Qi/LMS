b5816c31c81624adf580377dd77f69421cfd816a
jupiter <jupiter@zeus.com>
2021-07-18 01:32:49 +0800

    initial commit web and h5

---
+++ lms-web/src/test/java/com/jlu/lmsweb/LmsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlu.lmsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
