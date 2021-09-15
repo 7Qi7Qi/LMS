3dec2f8eee24132efaa4eb1552bc893fa3fad8a0
shilinchao <shilinchao@chinayasha.com>
2021-09-15 14:58:17 +0800

    vue init vite

--- lms-h5/src/App.vue
+++ lms-h5/src/App.vue
@@ -1,20 +1,21 @@
-<template>
-  <div id="app">
-    <el-button>1111</el-button>
-    <el-calendar>calendar</el-calendar>
-    <router-view> </router-view>
-  </div>
-</template>
-<script>
-export default {
-  name: 'App',
-
-  mounted() {
-    debugger;
-    console.log('App')
-  }
-}
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- lms-h5/src/main.js
+++ lms-h5/src/main.js
@@ -1,36 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
-import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import routes from './router/routes'
+import { createApp } from 'vue'
+import App from './App.vue'
 
 
 //Components
-import App from './App'
+createApp(ElementUI)
+createApp(App).mount('#app')
 
+const {createApp, h} = Vue
 
-Vue.config.productionTip = false
-Vue.use(ElementUI);
-
-/* eslint-disable no-new */
-const app = new Vue({
-  el: '#app',
-  components: { App },
-  render: h => h(this.ViewComponent),
-  data: {
-    currentRoute: window.location.pathname,
-  },
+const simpleRouter = {
+  data: () => ({
+    currentRoute: window.location.pathname
+  }),
   computed: {
-    ViewComponent() {
+    CurrentComponent() {
+      return routes[this.currentRoute] || 
       const matchView = routes[this.currentRoute];
       return matchView
-        ? require('./pages/' + matchView + '.vue')
-        : require('./pages/404.vue')
+          ? require('./pages/' + matchView + '.vue')
+          : require('./pages/404.vue')
     }
   },
-})
+}
 
 window.addEventListener('popstate', () => {
   app.currentRoute = window.location.pathname;
