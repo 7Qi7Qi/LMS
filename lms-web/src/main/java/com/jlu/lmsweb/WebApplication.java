ba18a97d4491eef0786954571cd16a4ccea80599
shilinchao <shilinchao@chinayasha.com>
2021-11-18 16:14:38 +0800

    add ProgressBarUtil

--- lms-web/src/main/java/com/jlu/lmsweb/WebApplication.java
+++ lms-web/src/main/java/com/jlu/lmsweb/WebApplication.java
@@ -1,5 +1,6 @@
 package com.jlu.lmsweb;
 
+import com.jlu.lmsweb.utils.ProgressBarUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,12 @@ public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
-        System.out.println("*****************************************LMS Started*****************************************");
+
+        ProgressBarUtil barUtil = new ProgressBarUtil.Builder().speed(10).interval(200).builder();
+        for (int i = 0; i <= 10; i++) {
+            barUtil.printProgress(i);
+        }
+        System.out.println("\n*****************************************LMS Started*****************************************");
     }
 
 }
--- lms-web/src/main/java/com/jlu/lmsweb/utils/ProgressBarUtil.java
+++ lms-web/src/main/java/com/jlu/lmsweb/utils/ProgressBarUtil.java
@@ -3,45 +3,72 @@ package com.jlu.lmsweb.utils;
 
 public class ProgressBarUtil {
 
+    // 进度条粒度
+    private static final int PROGRESS_SIZE = 50;
+    private final int RATE;
+    private final int SPEED;
+    private final long interval;
 
-    private String getChar(int num, char ch) {
-        return String.valueOf(ch).repeat(Math.max(0, num));
-    }
+    private String finish;
+    private String unFinish;
+    private String target;
 
-    public void printProgress(String title) throws InterruptedException {
-        int index = 0;
-        int BITE = 2;
-        int PROGRESS_SIZE = 50;
-        System.out.print(title);
+    public static class Builder {
 
-        String finish = getChar(0, '█');
-        String unFinish = getChar(PROGRESS_SIZE, '─');
-        String target = String.format("%3d%%[%s%s]", index, finish, unFinish);
-        System.out.print(target);
+        private int rate = 2;
+        private int speed = 1;
+        private long interval = 50L;
 
-        while (index <= 100) {
-            finish = getChar(index / BITE, '█');
-            unFinish = getChar(PROGRESS_SIZE - index / BITE, '─');
+        public Builder(){}
 
-            target = String.format("%3d%%├%s%s┤", index, finish, unFinish);
-            System.out.print(getChar(PROGRESS_SIZE + 6, '\b'));
-            System.out.print(target);
+        public Builder rate(int rate) {
+            this.rate = rate;
+            return this;
+        }
+        public Builder speed(int speed) {
+            this.speed = speed;
+            return this;
+        }
+        public Builder interval(int interval) {
+            this.interval = interval;
+            return this;
+        }
 
-            Thread.sleep(50);
-            index++;
+        public ProgressBarUtil builder() {
+            return new ProgressBarUtil(this);
         }
     }
 
-    public static void main(String[] args) {
-        ProgressBarUtil barUtil = new ProgressBarUtil();
+    private ProgressBarUtil(Builder builder) {
+        this.RATE = builder.rate;
+        this.SPEED = builder.speed;
+        this.interval = builder.interval;
+        this.refresh();
+    }
+
+    public void refresh() {
+        System.out.print("Progress:");
+        unFinish = getNChar(PROGRESS_SIZE, '─');
+        finish = getNChar(0, '█');
+        target = String.format("%3d%%[%s%s]", 0, finish, unFinish);
+        System.out.print(target);
+    }
+
+    public void printProgress(int index) {
+        unFinish = getNChar(PROGRESS_SIZE - index / RATE * SPEED, '─');
+        finish = getNChar(index / RATE * SPEED, '█');
+        if (index * SPEED > 100) {
+            return;
+        }
+        target = String.format("%3d%%├%s%s┤", index * SPEED, finish, unFinish);
+        System.out.print(getNChar(PROGRESS_SIZE + 6, '\b') + target);
         try {
-            for (int i = 0; i < 10; i++) {
-                barUtil.printProgress("ProgressBar: ");
-                System.out.println();
-            }
+            Thread.sleep(interval);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-
+    private String getNChar(int num, char ch){
+        return String.valueOf(ch).repeat(Math.max(0, num));
+    }
 }
