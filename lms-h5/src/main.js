bc1f71767464480bc016db2395be49d2fc8ff19d
jupiter <>
2022-03-19 22:14:18 +0800

    :card_file_box: some routes test

--- lms-h5/src/main.js
+++ lms-h5/src/main.js
@@ -1,6 +1,6 @@
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
-import routes from './router/routes'
+import routes from './routes'
 import { createApp, h} from 'vue'
 
 
--- lms-web/src/main/java/com/jlu/lmsweb/WebApplication.java
+++ lms-web/src/main/java/com/jlu/lmsweb/WebApplication.java
@@ -17,8 +17,6 @@ public class WebApplication {
         }
         System.out.println("\n*****************************************LMS Started*****************************************");
 
-//        ThreadPool threadPool = new ThreadPool();
-//        threadPool.start();
     }
 
 }
